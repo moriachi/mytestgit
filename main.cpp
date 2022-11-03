ffbca7ab26852700eda7c65f3f9deb8e767c317e
Mariia Tsymbal <mahachchan@gmail.com>
2022-11-03 21:45:07 +0100

    Lesson2: Add new function to add two numbers

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
+int add(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
     cout <<"Hello world"<<endl;
     cout <<"Hello from linux"<<endl;
+    cout <<"2+3="<< add(2,3)<<endl;
 
     return 0;
 }
